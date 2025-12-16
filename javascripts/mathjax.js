7d3c901906aac6c745a25c5c3b10e8da7fb854e2
shivam2003-dev <shivam2003-dev@users.noreply.github.com>
2025-12-16 16:32:45 +0000

    deploy: 21b946bc6d0305bef7fb25cec4e82a32eec6d012

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -1,14 +1,18 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["\\(", "\\)"], ["$", "$"]],
-    displayMath: [["\\[", "\\]"], ["$$", "$$"]],
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
     processEscapes: true,
     processEnvironments: true
   },
   options: {
-    skipHtmlTags: ["script", "noscript", "style", "textarea", "pre", "code"],
     ignoreHtmlClass: ".*|",
     processHtmlClass: "arithmatex"
   }
 };
+
+document$.subscribe(() => {
+  MathJax.startup.defaultReady();
+  MathJax.typesetPromise();
+})
 
